cb6b1d28febe076b10b53222a852537c46fb1ba1
xaata <xaata3007@mail.ru>
2021-03-04 20:36:10 +0900

    Lab1

--- myproxy.py
+++ myproxy.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from flask import request
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=['GET'])
 def hello_world():
-    return 'Hello IVT-17!'
+    msg="Hello IVT-17!"
+    if request.args.get('url'):
+        try:
+            r = requests.get(request.args['url'])
+            if r.status_code==200:
+                return r.content
+        except:
+            msg = f"Не удалось загрузить {request.args.get('url')}" 
+    return '<h1>' + msg + '''</h1>
+    <form method="get">
+    <input type="text" name="url">
+    <input type="submit">
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run()
